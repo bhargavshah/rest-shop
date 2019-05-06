ba9982e53cad839e919fa93c3980cc04f288b7e4
Bhargav Shah <bhargav3shah@gmail.com>
2019-05-06 15:02:04 +0530

    Update packages and add tweaks

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ const orderRoutes = require("./api/routes/orders");
 const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
-  "mongodb://node-shop:" +
+  "mongodb+srv://admin:" +
     process.env.MONGO_ATLAS_PW +
-    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
-  {
-    useMongoClient: true
-  }
+    "@bhargav-practice-agury.mongodb.net/test?retryWrites=true", {
+        useNewUrlParser: true
+    }
 );
 mongoose.Promise = global.Promise;
 
